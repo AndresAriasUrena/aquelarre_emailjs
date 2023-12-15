6a56fd5007e6db8f26a7eeb1afb6130602b5a093
Andres Arias <andresarias@Andress-MacBook-Pro.local>
2023-12-15 13:23:35 -0600

    hover tone applied, overflow-y auto applied. prod ready

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ const Subscribe = () => {
                 </div>
                 <button
                   type='submit'
-                  className='bg-red-600 hover:bg-purple-900 rounded-lg py-1 px-[8px] max-w-[60%] self-center'
+                  className='bg-red-600 hover:bg-[#4f36cc] rounded-lg py-1 px-[8px] max-w-[60%] self-center'
                   >
                     {loading ? 'Enviando...' : 'Suscribirme'}
                   </button>
@@ -111,13 +111,14 @@ export default function Home() {
     };
   }, []);
 
-  const treshold = 668;
+  const treshold = 500;
+  const treshold2 = 668;
 
   const imageUrl = windowWidth > treshold
     ? '/img/LunaLlenaDesktop.png'
     : '/img/LunaLlenaMovil2.png';
 
-  const subscibeUrl = windowWidth > treshold
+  const subscibeUrl = windowWidth > treshold2
   ? "img/suscribite-big.svg"
   : "img/suscribite.svg";
 
@@ -156,8 +157,8 @@ export default function Home() {
           className="relative max-h-96"
           src={imageUrl}
           alt="¿Qué harás la próxima luna llena?"
-          width={1251}
-          height={394}
+          width={770}
+          height={242}
           priority
         />
         <Image
@@ -168,7 +169,7 @@ export default function Home() {
           height={37}
           priority
         />
-        <div className='px-8 mt-8'>
+        <div className='px-8'>
           <Image
             className="relative text-center"
             src={subscibeUrl}
@@ -178,7 +179,7 @@ export default function Home() {
             priority
           />
         </div>
-        <div className='px-24 pt-4'>
+        <div className='px-24 py-4'>
           <Image
             className="relative text-center"
             src="/img/malasuerte.png"
@@ -192,88 +193,6 @@ export default function Home() {
       <div className='max-w-[80%] pb-8'>
         <Subscribe/>
       </div>
-
-
-      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div> */}
-
-      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
     </main>
   )
 }
