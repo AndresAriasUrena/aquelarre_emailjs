a9ef4e683294e5a82fad6222fe6e3f2791a6a94a
Andres Arias <andresarias@Andress-MacBook-Pro.local>
2024-01-09 23:40:08 -0600

    responsive hamburger menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -37,7 +37,6 @@ const Header = () => {
 
     function toggleMenu() {
         setshowMenu(!showMenu);
-        console.log('toggle');
         // navLinkDiv.classList.toggle('right-[0%]')
     }
 
@@ -48,87 +47,119 @@ const Header = () => {
       .then((newCategories) => setCategories(newCategories))
     }, [])
 
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+          <div
+            className={`nav-links fixed flex items-left p-20 flex-col justify-evenly bg-left bg-norepeat bg-cover z-20 h-full w-full text-white uppercase ${
+              showMenu ? 'right-0 translate-x-0' : 'right-[-100%] translate-x-full'
+            } overflow-hidden transition-transform ease-in-out`}
+            style={{ backgroundImage: `url(${menuBg})` }}
+          >
+            <Link onClick={toggleMenu} href='/'>
+              <Image unoptimized alt={'aquelarre logo'} height={125} width={250} className='cursor-pointer' src='/img/logo.png' />
+            </Link>
+            {navLinks.map((navlink, index) => (
+              <Link key={index} className='' href={navlink.href} onClick={toggleMenu}>
+                {navlink.label}
+              </Link>
+            ))}
+          </div>
     
-  return (
-    <div className='container mx-auto px-10 mb-8'>
-        {showMenu && (
-            <div style={{ backgroundImage: ` url(${menuBg})`}}
-                 className='nav-links fixed flex items-left p-20 flex-col justify-evenly bg-left bg-norepeat bg-cover z-20 h-full w-full text-white uppercase right-[100%] translate-x-full overflow-hidden'
-            >
-                <Link onClick={toggleMenu} href='/'>
-                    {/* <span className='cursor-pointer font-bold text-4xl text-white'>
-                        Aquelarre
-                    </span> */}
-                    <Image
-                        unoptimized
-                        alt={'aquelarre logo'}
-                        height={125}
-                        width={250}
-                        className='cursor-pointer'
-                        src='/img/logo.png'
-                    />
-                </Link>
-                {navLinks.map((navlink, index) => (
-                    <Link key={index} 
-                        className='' 
-                        href={navlink.href}
-                        onClick={toggleMenu} 
-                        >
-                            {navlink.label} 
-                    </Link>
-                ))}
-            </div>
-
-        )}
-        <div className='w-full inline-block py-4'>
+          <div className='w-full inline-block py-4'>
             <div className='hidden lg:flex justify-between text-white items-center uppercase'>
-                <Link href='/'>
-                    {/* <span className='cursor-pointer font-bold text-4xl text-white'>
-                        Aquelarre
-                    </span> */}
-                    <Image
-                        unoptimized
-                        alt={'aquelarre logo'}
-                        height={100}
-                        width={200}
-                        className='cursor-pointer'
-                        src='/img/logo.png'
-                    />
+              <Link href='/'>
+                <Image unoptimized alt={'aquelarre logo'} height={100} width={200} className='cursor-pointer' src='/img/logo.png' />
+              </Link>
+              {navLinks.map((navlink, index) => (
+                <Link key={index} className='' href={navlink.href}>
+                  {navlink.label}
                 </Link>
-                {navLinks.map((navlink, index) => (
-                    <Link key={index} className='' href={navlink.href}>
-                        {navlink.label}
-                    </Link>
-                ))}
-                {/* <Link href='/'><span>Noticias</span></Link>
-                <Link href='/'><span>Podcast: el oráculo</span></Link>
-                <Link href='/'><span>Agenda Cultural</span></Link>
-                <Link href='/'><span>Mercado</span></Link>
-                <Link href='/'><span>Oportunidades</span></Link> */}
+              ))}
             </div>
-            {/* <div className='hidden md:float-left md:contents'>
-                {categories.map((categoria) =>(
-                    <Link key={categoria.slug} href={`/category/${categoria.slug}`}>
-                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
-                            {categoria.name}
-                        </span>
-                    </Link>
-                ))}
-            </div> */}
-
+    
             <button onClick={toggleMenu} className='fixed right-5 lg:hidden z-40'>
-                <Image
-                    unoptimized
-                    alt='menu movil'
-                    height={85}
-                    width={35}
-                    className='align-middle'
-                    src='/img/icono-menu.png'
-                />
+              <Image unoptimized alt='menu movil' height={85} width={35} className='align-middle' src='/img/icono-menu.png' />
             </button>
+          </div>
         </div>
-    </div>
-  );
+      );
+
+    
+//   return (
+//     <div className='container mx-auto px-10 mb-8'>
+//         {showMenu && (
+//             <div style={{ backgroundImage: ` url(${menuBg})`}}
+//                  className='nav-links fixed flex items-left p-20 flex-col justify-evenly bg-left bg-norepeat bg-cover z-20 h-full w-full text-white uppercase right-[100%] translate-x-full overflow-hidden'
+//             >
+//                 <Link onClick={toggleMenu} href='/'>
+//                     {/* <span className='cursor-pointer font-bold text-4xl text-white'>
+//                         Aquelarre
+//                     </span> */}
+//                     <Image
+//                         unoptimized
+//                         alt={'aquelarre logo'}
+//                         height={125}
+//                         width={250}
+//                         className='cursor-pointer'
+//                         src='/img/logo.png'
+//                     />
+//                 </Link>
+//                 {navLinks.map((navlink, index) => (
+//                     <Link key={index} 
+//                         className='' 
+//                         href={navlink.href}
+//                         onClick={toggleMenu} 
+//                         >
+//                             {navlink.label} 
+//                     </Link>
+//                 ))}
+//             </div>
+
+//         )}
+//         <div className='w-full inline-block py-4'>
+//             <div className='hidden lg:flex justify-between text-white items-center uppercase'>
+//                 <Link href='/'>
+//                     {/* <span className='cursor-pointer font-bold text-4xl text-white'>
+//                         Aquelarre
+//                     </span> */}
+//                     <Image
+//                         unoptimized
+//                         alt={'aquelarre logo'}
+//                         height={100}
+//                         width={200}
+//                         className='cursor-pointer'
+//                         src='/img/logo.png'
+//                     />
+//                 </Link>
+//                 {navLinks.map((navlink, index) => (
+//                     <Link key={index} className='' href={navlink.href}>
+//                         {navlink.label}
+//                     </Link>
+//                 ))}
+//             </div>
+//             {/* <div className='hidden md:float-left md:contents'>
+//                 {categories.map((categoria) =>(
+//                     <Link key={categoria.slug} href={`/category/${categoria.slug}`}>
+//                         <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+//                             {categoria.name}
+//                         </span>
+//                     </Link>
+//                 ))}
+//             </div> */}
+
+//             <button onClick={toggleMenu} className='fixed right-5 lg:hidden z-40'>
+//                 <Image
+//                     unoptimized
+//                     alt='menu movil'
+//                     height={85}
+//                     width={35}
+//                     className='align-middle'
+//                     src='/img/icono-menu.png'
+//                 />
+//             </button>
+//         </div>
+//     </div>
+//   );
 }
 
 export default Header
